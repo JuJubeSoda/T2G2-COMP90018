4f87419415d16f040ffe4633af17e819ef793c27
Jiamin Tong <jiamintong@JiamindeMacBook-Air.local>
2025-10-21 14:04:55 +1100

    fixing

--- code/plant-backend/src/main/java/org/unimelb/ai/service/PlantAIService.java
+++ code/plant-backend/src/main/java/org/unimelb/ai/service/PlantAIService.java
@@ -5,6 +5,7 @@ import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
 import java.util.*;
 
 @Service
